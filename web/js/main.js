f4aded1e6afc9944de602c132b61af89feccd837
ArtjomM <artjommol001@gmail.com>
2020-03-19 14:37:47 +0200

    начало создания проекта. День 1. Конец дня 19.03.2020

---
+++ web/js/main.js
@@ -0,0 +1,26 @@
+document.getElementById('enter-menu').onclick = function () {
+    toogleMenuActive('enter-menu');
+}
+document.getElementById('sysout').onclick = function () {
+    toogleMenuActive('sysout');
+}
+document.getElementById('active').onclick = function () {
+    toogleMenuActive('active');
+}
+document.getElementById('link').onclick = function () {
+    toogleMenuActive('link');
+}
+
+function toogleMenuActive(elementId) {
+    let activeElement = document.getElementById(elementId);
+    let passiveElements = document.getElementsByClassName('nav-item');
+    for (let i = 0; i < passiveElements.length; i++) {
+        if (activeElement === passiveElements[i]) {
+            passiveElements[i].classList.add("active-menu");
+        } else {
+            if (passiveElements[i].classList.contains('active-menu')) {
+                passiveElements[i].classList.remove('active-menu');
+            }
+        }
+    }
+}
